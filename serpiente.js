6b1b51623033e99934bbd1fc153a387c2ae07d27
ianm1610 <ianmetem1610@gmail.com>
2026-05-16 20:26:16 -0500

    agregando movimientos,comida y crecimiento

--- serpiente.js
+++ serpiente.js
@@ -1,38 +1,178 @@
 const canvas = document.getElementById("canvasJuego"); 
 const ctx = canvas.getContext("2d"); 
 
+
+const elementoPuntaje = document.getElementById("puntaje");
+const elementoEstado = document.getElementById("estado");
+const elementoMensaje = document.getElementById("mensaje");
+
 const TAMANIO_CELDA = 25; // Tamaño de cada cuadrado de la cuadrícula 
 
 // ===================================
-// CREACIÓN DE LA SERPIENTE 
-// ==============================
-// Cada objeto representa una parte. El primer elemento en el que est en 0 será la cabeza.
-const serpiente = [
-  { x: 0, y: 10 }, //  esta es la Cabeza
-  { x: 0, y: 11 },
-  { x: 0, y: 12 },
-  { x: 0, y: 13 },
-  { x: 0, y: 14 }
+// VARIABLES GLOBALES DEL JUEGO
+// ===================================
+// cada objeto representa una parte. El primer elemento [0] es la cabeza.
+let serpiente = [
+  { x: 10, y: 10 }, 
+  { x: 10, y: 11 },
+  { x: 10, y: 12 },
+  { x: 10, y: 13 },
+  { x: 10, y: 14 }
 ];
 
+// Variables de Control Automática
+let intervaloSerpiente = null; 
+let direccionActual = "arriba"; // Dirección inicial por defecto
+let puntaje = 0;
+
+// la estructura para almacenar la posición de la comida 
+let comida = { x: 0, y: 0 };
+
+
+generarComidaAleatoria();
 dibujarTodo();
 
+// =========
+// FUNCIONES DE MOVIMIENTO 
+// ==========
+
+function moverDerecha() {
+  const cabeza = serpiente[0]; 
+  const nuevaCabeza = { x: cabeza.x + 1, y: cabeza.y }; 
+  serpiente.unshift(nuevaCabeza); 
+  procesarAvanzado();
+}
+
+function moverIzquierda() { 
+  const cabeza = serpiente[0];
+  const nuevaCabeza = { x: cabeza.x - 1, y: cabeza.y };
+  serpiente.unshift(nuevaCabeza);
+  procesarAvanzado();
+}
+
+function moverArriba() { 
+  const cabeza = serpiente[0];
+  const nuevaCabeza = { x: cabeza.x, y: cabeza.y - 1 };
+  serpiente.unshift(nuevaCabeza);
+  procesarAvanzado();
+}
+
+function moverAbajo() {
+  const cabeza = serpiente[0];
+  const nuevaCabeza = { x: cabeza.x, y: cabeza.y + 1 };
+  serpiente.unshift(nuevaCabeza);
+  procesarAvanzado();
+}
+
+/**esto es  para determinar si la serpiente reduce su cola o crece*/
+function procesarAvanzado() {
+  // la cabeza coincide con la posición de la comida
+  if (atrapaComida()) { 
+    puntaje += 10; 
+    elementoPuntaje.textContent = puntaje;
+    generarComidaAleatoria(); // Generamos una nueva comida en otra posición
+  } else {
+    // Si no come, se elimina el último elemento 
+    serpiente.pop(); 
+  }
+}
+
+// ====================================
+// DIRECCIÓN Y FLUIDO 
+// ======================
+
+function cambiarDireccion(nuevaDireccion) { 
+  // Evitamos que la serpiente se mate sola cuando gira a 180 grados 
+  if (nuevaDireccion === "derecha" && direccionActual !== "izquierda") direccionActual = "derecha";
+  if (nuevaDireccion === "izquierda" && direccionActual !== "derecha") direccionActual = "izquierda";
+  if (nuevaDireccion === "arriba" && direccionActual !== "abajo") direccionActual = "arriba";
+  if (nuevaDireccion === "abajo" && direccionActual !== "arriba") direccionActual = "abajo";
+}
+
+function moverSerpiente() { 
+  console.log("moviendo"); // Validación en consola requerida 
+
+  // Dependiendo de la variable de direccion se ejecuta el movimiento 
+  if (direccionActual === "derecha") moverDerecha(); 
+  else if (direccionActual === "izquierda") moverIzquierda(); 
+  else if (direccionActual === "arriba") moverArriba(); 
+  else if (direccionActual === "abajo") moverAbajo();
+
+  dibujarTodo(); 
+}
+
+function iniciarJuego() { 
+  if (intervaloSerpiente === null) { // Evita duplicar los intervalos si  se presionam muchas veces
+    intervaloSerpiente = setInterval(moverSerpiente, 200); 
+    elementoEstado.textContent = "Jugando";
+    elementoMensaje.textContent = "¡Juego en marcha! Utiliza las flechas para guiar a la serpiente.";
+  }
+}
+
+function pausarJuego() { 
+  if (intervaloSerpiente !== null) {
+    clearInterval(intervaloSerpiente); // Parte 6: Detiene el bucle 
+    intervaloSerpiente = null;
+    elementoEstado.textContent = "Pausado";
+    elementoMensaje.textContent = "Juego pausado. Presiona Iniciar para continuar.";
+  }
+}
+
+function reiniciarJuego() {
+  pausarJuego();
+  serpiente = [
+    { x: 10, y: 10 }, 
+    { x: 10, y: 11 },
+    { x: 10, y: 12 },
+    { x: 10, y: 13 },
+    { x: 10, y: 14 }
+  ];
+  direccionActual = "arriba";
+  puntaje = 0;
+  elementoPuntaje.textContent = puntaje;
+  elementoEstado.textContent = "Listo";
+  elementoMensaje.textContent = "Presiona iniciar para comenzar.";
+  generarComidaAleatoria();
+  dibujarTodo();
+}
+
 // ==========================================
-// FUNCIONES DEL DIBUJO
+// CONTROL DE LA COMIDA Y COLISIÓN
+// ==========================================
+
+/**
+ * Calcula coordenadas aleatorias lógicas para posicionar la manzana (
+ */
+function generarComidaAleatoria() {
+  // nos da la cantidad máxima de cuadros en X y en Y basada en el tamaño del Canvas 
+  const maxLineasX = canvas.width / TAMANIO_CELDA; // 600 / 25 = 24 celdas 
+  const maxLineasY = canvas.height / TAMANIO_CELDA; 
+
+  // esto de aqui nos da un número entero entre 0 y el límite 
+  comida.x = Math.floor(Math.random() * maxLineasX); 
+  comida.y = Math.floor(Math.random() * maxLineasY); 
+}
+
+/**
+ * Evalúa si la cabeza de la serpiente está tocando el ítem
+ */
+function atrapaComida() { 
+  const cabeza = serpiente[0];
+  return cabeza.x === comida.x && cabeza.y === comida.y; 
+}
+
+// ==========================================
+// FUNCIONES GENERALES DEL RENDERIZADO
 // ==========================================
 
 function limpiarCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-/*
- * Dibuja la cuadrícula del tablero usando líneas verticales y horizontales
- */
 function dibujarTablero() { 
   ctx.strokeStyle = "#1e293b"; 
   ctx.lineWidth = 1;
 
-  // --- LÍNEAS VERTICALES ---
   for (let x = 0; x <= canvas.width; x += TAMANIO_CELDA) {
     ctx.beginPath(); 
     ctx.moveTo(x, 0); 
@@ -40,7 +180,6 @@ function dibujarTablero() {
     ctx.stroke(); 
   }
 
-  // --- LÍNEAS HORIZONTALES ---
   for (let y = 0; y <= canvas.height; y += TAMANIO_CELDA) { 
     ctx.beginPath(); 
     ctx.moveTo(0, y); 
@@ -49,50 +188,38 @@ function dibujarTablero() {
   }
 }
 
-/** Función para pintar un cuadrado basado en coordenadas de cuadrícula*/
 function pintarParte(lineaX, lineaY, colorRelleno) { 
-  // Calcular la posición de los píxeles dentro del canvas 
   const xReal = lineaX * TAMANIO_CELDA; 
   const yReal = lineaY * TAMANIO_CELDA; 
 
-  // Configurar color de relleno y pintar el cuadrado 
   ctx.fillStyle = colorRelleno; 
   ctx.fillRect(xReal, yReal, TAMANIO_CELDA, TAMANIO_CELDA);
 
-  // Configurar color del borde y dibujarlo 
   ctx.strokeStyle = "#020617";
   ctx.lineWidth = 2;
   ctx.strokeRect(xReal, yReal, TAMANIO_CELDA, TAMANIO_CELDA); 
 }
 
-/** Recorre el arreglo de la serpiente para dibujarla por completo*/
 function pintarSerpiente() { 
-  // Recoremos cada una de las partes del arreglo 
   for (let i = 0; i < serpiente.length; i++) {
-    //  La cabeza (índice 0) tiene un color diferente al cuerpo 
     if (i === 0) {
-      pintarParte(serpiente[i].x, serpiente[i].y, "#facc15"); // Amarillo para la Cabeza 
+      pintarParte(serpiente[i].x, serpiente[i].y, "#facc15"); // Cabeza Amarilla 
     } else {
-      pintarParte(serpiente[i].x, serpiente[i].y, "#ef4444"); // Rojo para el Cuerpo 
+      pintarParte(serpiente[i].x, serpiente[i].y, "#ef4444"); // Cuerpo Rojo
     }
   }
+}
+
+/**
+ * Pinta la comida en su posicion generad
+ */
+function pintarComida() { 
+  pintarParte(comida.x, comida.y, "#22c55e"); 
 }
 
 function dibujarTodo() {
   limpiarCanvas(); 
   dibujarTablero(); 
-
-  // --- SECCIÓN DE PRUEBAS OBLIGATORIAS  ---
-  // descomenta esta líneas si se necesita mostrar los bloques de prueba individuales:
-  /*
-  pintarParte(5, 5, "#ef4444");   // PRUEBA 1: Bloque en (5,5) 
-  pintarParte(10, 2, "#ef4444");  // PRUEBA 2: Bloque en (10,2) [
-  pintarParte(12, 23, "#ef4444"); // PRUEBA 3: Pegado al borde inferior (En canvas de 600px, 600/25 = 24 celdas máximo, la última es 23)
-  pintarParte(23, 10, "#ef4444"); // PRUEBA 4: Pegado al borde derecho 
-  pintarParte(0, 15, "#ef4444");  // PRUEBA 5: Pegado al borde izquierdo
-  pintarParte(23, 23, "#ef4444"); // PRUEBA 6: En una esquina (esquina inferior derecha)
-  */
-
-  // PASO 5: invocamos la función de la serpiente armada (esto borra los individuales automáticos) 
+  pintarComida(); 
   pintarSerpiente(); 
 }
